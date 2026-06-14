1a41103730399c78ab227e137ccc5c6734ac8008
tomlinovic <150364007+tomlinovic@users.noreply.github.com>
2026-06-14 18:44:24 +0200

    Update database.php

--- config/database.php
+++ config/database.php
@@ -17,7 +17,8 @@ return [
     |
     */
 
-    'default' => env('DB_CONNECTION', 'sqlite'),
+    'default' => env('DB_CONNECTION', 'mysql'),
+
 
     /*
     |--------------------------------------------------------------------------
