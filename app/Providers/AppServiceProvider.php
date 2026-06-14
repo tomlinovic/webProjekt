132dd0f6b2959f2123da8385fb424fb82e2947a1
tomlinovic <150364007+tomlinovic@users.noreply.github.com>
2026-06-15 01:28:07 +0200

    Update AppServiceProvider.php

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,10 +3,20 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL;
 
-class AppServiceProvider extends Serivce Provider
+class AppServiceProvider extends ServiceProvider
 {
+    /**
+     * Register any application services.
+     */
+    public function register(): void
+    {
+        //
+    }
+
+    /**
+     * Bootstrap any application services.
+     */
     public function boot(): void
     {
         if (config('app.env') === 'production') {
