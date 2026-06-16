5e447dce8074603d9acfe231909b6e9b373b8867
tomlinovic <150364007+tomlinovic@users.noreply.github.com>
2026-06-16 16:55:49 +0200

    Update login.blade.php

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,19 +1,48 @@
 <x-layout>
-
 <div class="auth-container">
     <h1>Login</h1>
 
-    <form method="POST" action="/login">
+    {{-- Laravel serverske greške (npr. pogrešna lozinka) --}}
+    @if ($errors->any())
+        <div id="login-errors">
+            @foreach ($errors->all() as $error)
+                <div class="error">{{ $error }}</div>
+            @endforeach
+        </div>
+    @else
+        <div id="login-errors"></div>
+    @endif
+
+    <form method="POST" action="/login" id="login-form">
         @csrf
 
-        <input type="email" name="email" placeholder="Email" required>
+        <input
+            type="email"
+            id="login-email"
+            name="email"
+            placeholder="Email"
+            value="{{ old('email') }}"
+            required
+        >
 
-        <input type="password" name="password" placeholder="Password" required>
+        <div class="password-wrapper">
+            <input
+                type="password"
+                id="login-password"
+                name="password"
+                placeholder="Password"
+                required
+            >
+            <button type="button" class="toggle-password" data-target="login-password">
+                ◡
+            </button>
+        </div>
 
-        <button type="submit">Login</button>
+        <button type="submit" id="login-btn">Login</button>
     </form>
 
     <p>No account? <a href="/register">Register</a></p>
 </div>
 
+@vite(['resources/css/register.css', 'resources/js/login.js'])
 </x-layout>
